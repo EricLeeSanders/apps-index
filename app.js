4622ff8f5ba9e8087cf5d1f0e606f7f9628afbe8
Eric Sanders <EricLeeSanders@gmail.com>
2017-07-13 19:48:17 -0400

    fixed firefox bug

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
      $(document).ready(function() {
          $('.card').hover(function() {
              $(this).toggleClass('flip');
-
          });
+
+         $('.card').removeClass('flip');
+
      });
  }(jQuery));
 
